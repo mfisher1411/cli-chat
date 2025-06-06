268a975f1fc669528a2f9d8402089d4465f4a889
mfisher1411 <matthewfisher1411@gmail.com>
2025-06-06 15:20:42 +0500

    deploy auth_pg
    fix! docker-compose.prod.yaml

--- services/auth_postgres/migrator/migrator-prod.sh
+++ services/auth_postgres/migrator/migrator-prod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export MIGRATION_DIR="./migrations"
-export MIGRATION_DSN="host=auth_pg port=5432 dbname=$POSTGRES_DB user=$POSGRES_USER password=$POSTGRES_PASSWORD sslmode=disable"
+export MIGRATION_DSN="host=auth_pg port=5432 dbname=$POSTGRES_DB user=$POSTGRES_USER password=$POSTGRES_PASSWORD sslmode=disable"
 
 sleep 2 && goose -dir "${MIGRATION_DIR}" postgres "${MIGRATION_DSN}" up -v
